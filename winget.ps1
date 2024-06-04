6e4ec9c18d6e02483c34a005da5bc6479162c942
Schooi <118210923+SchooiCodes@users.noreply.github.com>
2024-06-04 21:29:21 +0300

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -1,5 +1,171 @@
 #Code by Chris Titus
-Try {
+function Get-WinUtilWingetLatest {
+    <#
+    .SYNOPSIS
+        Uses GitHub API to check for the latest release of Winget.
+    .DESCRIPTION
+        This function grabs the latest version of Winget and returns the download path to Install-WinUtilWinget for installation.
+    #>
+
+    Try{
+        # Grabs the latest release of Winget from the Github API for the install process.
+        $response = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/Winget-cli/releases/latest" -Method Get -ErrorAction Stop
+        $latestVersion = $response.tag_name #Stores version number of latest release.
+        $licenseWingetUrl = $response.assets.browser_download_url[0] #Index value for License file.
+        Write-Host "Latest Version:`t$($latestVersion)`n"
+        $assetUrl = $response.assets.browser_download_url[2] #Index value for download URL.
+        Invoke-WebRequest -Uri $licenseWingetUrl -OutFile $ENV:TEMP\License1.xml
+        # The only pain is that the msixbundle for winget-cli is 246MB. In some situations this can take a bit, with slower connections.
+        Invoke-WebRequest -Uri $assetUrl -OutFile $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle
+    }
+    Catch{
+        throw [WingetFailedInstall]::new('Failed to get latest Winget release and license')
+    }
+}
+function Get-WinUtilWingetPrerequisites {
+    <#
+    .SYNOPSIS
+        Downloads the Winget Prereqs.
+    .DESCRIPTION
+        Downloads Prereqs for Winget. Version numbers are coded as variables and can be updated as uncommonly as Microsoft updates the prereqs.
+    #>
+
+    # I don't know of a way to detect the prereqs automatically, so if someone has a better way of defining these, that would be great.
+    # Microsoft.VCLibs version rarely changes, but for future compatibility I made it a variable.
+    $versionVCLibs = "14.00"
+    $fileVCLibs = "https://aka.ms/Microsoft.VCLibs.x64.${versionVCLibs}.Desktop.appx"
+    # Write-Host "$fileVCLibs"
+    # Microsoft.UI.Xaml version changed recently, so I made the version numbers variables.
+    $versionUIXamlMinor = "2.8"
+    $versionUIXamlPatch = "2.8.6"
+    $fileUIXaml = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v${versionUIXamlPatch}/Microsoft.UI.Xaml.${versionUIXamlMinor}.x64.appx"
+    # Write-Host "$fileUIXaml"
+
+    Try{
+        Write-Host "Downloading Microsoft.VCLibs Dependency..."
+        Invoke-WebRequest -Uri $fileVCLibs -OutFile $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx
+        Write-Host "Downloading Microsoft.UI.Xaml Dependency...`n"
+        Invoke-WebRequest -Uri $fileUIXaml -OutFile $ENV:TEMP\Microsoft.UI.Xaml.x64.appx
+    }
+    Catch{
+        throw [WingetFailedInstall]::new('Failed to install prerequsites')
+    }
+}
+function Install-WinUtilChoco {
+
+    <#
+
+    .SYNOPSIS
+        Installs Chocolatey if it is not already installed
+
+    #>
+
+    try {
+        Write-Host "Checking if Chocolatey is Installed..."
+
+        if((Test-WinUtilPackageManager -choco) -eq "installed") {
+            return
+        }
+
+        Write-Host "Seems Chocolatey is not installed, installing now."
+        Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
+        powershell choco feature enable -n allowGlobalConfirmation
+
+    }
+    Catch {
+        Write-Host "===========================================" -Foregroundcolor Red
+        Write-Host "--     Chocolatey failed to install     ---" -Foregroundcolor Red
+        Write-Host "===========================================" -Foregroundcolor Red
+    }
+
+}
+Function Install-WinUtilProgramWinget {
+
+    <#
+    .SYNOPSIS
+        Manages the provided programs using Winget
+
+    .PARAMETER ProgramsToInstall
+        A list of programs to manage
+
+    .PARAMETER manage
+        The action to perform on the programs, can be either 'Installing' or 'Uninstalling'
+
+    .NOTES
+        The triple quotes are required any time you need a " in a normal script block.
+    #>
+
+    param(
+        $ProgramsToInstall,
+        $manage = "Installing"
+    )
+
+    $x = 0
+    $count = $($ProgramsToInstall -split ",").Count
+
+    Write-Progress -Activity "$manage Applications" -Status "Starting" -PercentComplete 0
+
+    Foreach ($Program in $($ProgramsToInstall -split ",")){
+
+        Write-Progress -Activity "$manage Applications" -Status "$manage $Program $($x + 1) of $count" -PercentComplete $($x/$count*100)
+        if($manage -eq "Installing"){
+            # Install package via ID, if it fails try again with different scope and then with an unelevated prompt. 
+            # Since Install-WinGetPackage might not be directly available, we use winget install command as a workaround.
+            # Winget, not all installers honor any of the following: System-wide, User Installs, or Unelevated Prompt OR Silent Install Mode.
+            # This is up to the individual package maintainers to enable these options. Aka. not as clean as Linux Package Managers.
+            try {
+                $status = $(Start-Process -FilePath "winget" -ArgumentList "install --id $Program --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru).ExitCode
+                if($status -ne 0){
+                    Write-Host "Attempt with User scope"
+                    $status = $(Start-Process -FilePath "winget" -ArgumentList "install --id $Program --scope user --silent --accept-source-agreements --accept-package-agreements" -Wait -PassThru).ExitCode
+                    if($status -ne 0){
+                        Write-Host "Attempt with Unelevated prompt"
+                        $status = $(Start-Process -FilePath "powershell" -ArgumentList "-Command Start-Process winget -ArgumentList 'install --id $Program --silent --accept-source-agreements --accept-package-agreements' -Verb runAsUser" -Wait -PassThru).ExitCode
+                        if($status -ne 0){
+                            Write-Host "Failed to install $Program."
+                        } else {
+                            Write-Host "$Program installed successfully with Unelevated prompt."
+                        }
+                    } else {
+                        Write-Host "$Program installed successfully with User scope."
+                    }
+                } else {
+                    Write-Host "$Program installed successfully."
+                }
+            } catch {
+                Write-Host "Failed to install $Program due to an error: $_"
+            }
+        }
+        if($manage -eq "Uninstalling"){
+            # Uninstall package via ID using winget directly.
+            try {
+                $status = $(Start-Process -FilePath "winget" -ArgumentList "uninstall --id $Program --silent" -Wait -PassThru).ExitCode
+                if($status -ne 0){
+                    Write-Host "Failed to uninstall $Program."
+                } else {
+                    Write-Host "$Program uninstalled successfully."
+                }
+            } catch {
+                Write-Host "Failed to uninstall $Program due to an error: $_"
+            }
+        }
+        $X++
+    }
+
+    Write-Progress -Activity "$manage Applications" -Status "Finished" -Completed
+}
+function Install-WinUtilWinget {
+    <#
+
+    .SYNOPSIS
+        Installs Winget if it is not already installed.
+
+    .DESCRIPTION
+        This function will download the latest version of Winget and install it. If Winget is already installed, it will do nothing.
+    #>
+    $isWingetInstalled = Test-WinUtilPackageManager -winget
+
+    Try {
         if ($isWingetInstalled -eq "installed") {
             Write-Host "`nWinget is already installed.`r" -ForegroundColor Green
             return
@@ -51,8 +217,7 @@ Try {
             throw [WingetFailedInstall]::new('Failed to install!')
         }
     }
-
-
+}
 # get latest download url
 #$URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 #$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
